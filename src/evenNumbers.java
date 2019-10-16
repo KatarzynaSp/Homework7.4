05d0fb5be62fabe8fc56c1067ded9af27d8de841
Katarzyna <katarzyna.panek111@gmail.com>
2019-10-16 22:24:59 +0200

    zad 4 wersja 0.

---
+++ src/evenNumbers.java
@@ -0,0 +1,11 @@
+public class evenNumbers {
+    public static void main(String[] args) {
+
+        int sum = 0;
+        for (int i = 0; i <= 100; i += 2) {
+            System.out.println(i);
+            sum = sum + i;
+        }
+        System.out.println(sum);
+    }
+}
